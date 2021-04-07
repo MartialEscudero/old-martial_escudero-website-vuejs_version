4d764561ec9f0220aca2fcc87f707d4ebebb071f
Escudero Martial <martial.escudero@gmail.com>
2021-04-07 12:43:17 +0200

    add CV

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,22 +1,293 @@
 <template>
-  <div class="flex h-screen">
-    <div class="m-auto text-md xl:mx-auto">
+  <div class="fxl:mt-44 md:mt-36 sm:mt-28 sm:mb-7">
+    <h1 class="container mx-auto lg:text-left sm:text-center text-5xl mb-2">
       CV
+    </h1>
+    <div class="container mx-auto grid xl:grid-cols-2 sm:grid-cols-1 gap-x-0 gap-y-20 mt-32 xl:mb-10">
+      <div class="text-center text-4xl font-bold">Moi.</div>
+      <div class="text-justify">
+        <p>
+          Actuellement en <a class="lien" target="_blank" href="https://www.sciences.unilim.fr/tic/deust-webmaster-metiers-internet/">DEUST Webmaster & Métiers de l'Internet</a> à l’Université de Limoges,
+          je suis diplômé d'un <a class="lien" target="_blank" href="http://www.lyceecaraminot.fr/">Baccalauréat Scientifique option et spécialité Sciences de l'Ingénieur.</a>
+        </p>
+        <p class="mb-10 mt-4">
+          Étudiant investie et dynamique, je possède une très bonne connaissance du monde du web et des nouvelles technologies.
+        </p>
+        <div id="wrapper">
+          <a class="rounded-full" target="_blank" href="docs/CV - Martial ESCUDERO.pdf">
+            <button class="btn bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-full inline-flex items-center">
+              <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+              <span>Télécharger mon CV</span>
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto grid xl:grid-cols-2 sm:grid-cols-1 gap-x-0 gap-y-20 mt-32 xl:mb-10">
+      <div class="text-center text-4xl font-bold">Diplôme.</div>
+      <div class="text-justify">
+        <article>
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Université de Limoges, Faculté Sciences Et Techniques</span> | DEUST</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">SEPTEMBRE 2019 - LIMOGES</span></div>
+          <p class="text-sm">Préparation du diplôme DEUST Webmaster & Métiers de l’Internet.</p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Lycée Pierre Caraminot</span> | Baccalauréat</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">JUILLET 2019 - ÉGLETONS</span></div>
+          <p class="text-sm">Baccalauréat Scientifique spécialité et option Sciences de l’Ingénieur.</p>
+        </article>
+      </div>
+    </div>
+    <div class="container mx-auto grid xl:grid-cols-2 sm:grid-cols-1 gap-x-0 gap-y-20 mt-32 xl:mb-10">
+      <div class="text-center text-4xl font-bold">Expériences.</div>
+      <div class="text-justify">
+        <article>
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Site Web Personnel</span> | Cadre Personnel</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">AVRIL 2021</span></div>
+          <p class="text-sm">Refonte totale de mon site www.martialescudero.com, développement en utilisant : Vue.js, VueX, Node.js, Strapi et Tailwind CSS.</p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">HortusBox</span> | Stage</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">JANVIER 2021</span></div>
+          <p class="text-sm">Stage de 2 mois dans le cadre de mon DEUST Webmaster & Métiers de l’Internet. 
+            <br><br>Missions :
+            <br>- Intégration de maquettes
+            <br>- Amélioration du parcours client
+            <br>- Rajout de nouveaux designs du site
+            <br>- Développement des nouvelles features
+            <br>- Développement d'un outil d'édition et de génération d'un book en PDF
+            <br><br>Techno : Vue.js / Vuex / Vuetify / Nuxt.js / Strapi / Node.js / Puppeteer
+          </p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Office de Tourisme Ventadour-Egletons-Monédières</span> | Stage</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">MARS 2020 - ÉGLETONS</span></div>
+          <p class="text-sm">Stage de 4 semaines dans le cadre de mon DEUST Webmaster & Métiers de l’Internet. 
+            <br><br>Missions :
+            <br>- Développement web et préconisations pour le site
+            <br>- Audit, analyse du site web
+            <br>- Community management
+            <br>- Réalisation de photos et vidéos à destination des réseaux sociaux
+          </p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Site Web Personnel V1</span> | Cadre Personnel</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">MARS 2020</span></div>
+          <p class="text-sm">Développement de mon site internet www.martialescudero.com.</p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">COM COM Service Ordures Ménagères</span> | Adjoint Technique</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">JUILLET 2018 à JANVIER 2021 (par période) - ÉGLETONS</span></div>
+          <p>Emploi saisonnier durant chaque période de vacances scolaires.</p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">PROMAN</span> | Intérimaire</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">AOÛT 2019 à JUIN 2020 (par période) - ÉGLETONS</span></div>
+          <p class="text-sm">Emploi saisonnier durant chaque période de vacances scolaires.</p>
+        </article>
+        <article class="mt-10">
+          <p class="xl:text-xl sm:text-lg text-left"><span class="font-bold">Réalisations</span> | Cadre personnel</p>
+          <div class="italic mt-2 mb-5 text-xs"><span class="date">2019</span></div>
+          <p class="text-sm">Sous l’entité « Pure. Production » du Graphisme et des Vidéo-Clips.</p>
+        </article>
+      </div>
+    </div>
+    <div class="container mx-auto grid xl:grid-cols-2 sm:grid-cols-1 gap-x-0 gap-y-20 mt-32 xl:mb-10">
+      <div class="text-center text-4xl font-bold">Compétences.</div>
+      <div>
+        <article class="text-justify">
+        <h1 class="text-left font-bold text-2xl">Languages</h1>
+        <hr class="my-6">
+        <section v-for="language in languages" :key="language.item">
+          <div class="relative pt-1">
+            <div class="flex mb-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 rounded-full">
+                  {{language.name}}
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  {{language.pourcent}}%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-300">
+              <div :style="'width:'+language.pourcent+'%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-500"></div>
+            </div>
+          </div>
+        </section>
+      </article>
+      <article class="text-justify mt-10">
+        <h1 class="text-left font-bold text-2xl">Framework</h1>
+        <hr class="my-6">
+        <section v-for="framework in frameworks" :key="framework.item">
+          <div class="relative pt-1">
+            <div class="flex mb-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 rounded-full">
+                  {{framework.name}}
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  {{framework.pourcent}}%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-300">
+              <div :style="'width:'+framework.pourcent+'%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-500"></div>
+            </div>
+          </div>
+        </section>
+      </article>
+      <article class="text-justify mt-10">
+        <h1 class="text-left font-bold text-2xl">CMS</h1>
+        <hr class="my-6">
+        <section v-for="cms in cmss" :key="cms.item">
+          <div class="relative pt-1">
+            <div class="flex mb-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 rounded-full">
+                  {{cms.name}}
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  {{cms.pourcent}}%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-300">
+              <div :style="'width:'+cms.pourcent+'%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-500"></div>
+            </div>
+          </div>
+        </section>
+      </article>
+      <article class="text-justify mt-10">
+        <h1 class="text-left font-bold text-2xl">Design</h1>
+        <hr class="my-6">
+        <section v-for="design in designs" :key="design.item">
+          <div class="relative pt-1">
+            <div class="flex mb-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 rounded-full">
+                  {{design.name}}
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  {{design.pourcent}}%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-gray-300">
+              <div :style="'width:'+design.pourcent+'%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-500"></div>
+            </div>
+          </div>
+        </section>
+      </article>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    languages: [
+      {name: "HTML", pourcent: 99 },
+      {name: "CSS", pourcent: 85 },
+      {name: "JavaScript", pourcent: 70 },
+      {name: "jQuery", pourcent: 70 },
+      {name: "Ajax", pourcent: 60 },
+      {name: "PHP", pourcent: 50 },
+      {name: "MySQL / PhpMyAdmin", pourcent: 50 },
+      {name: "XML", pourcent: 50 },
+    ],
+    frameworks: [
+      {name: "Vue.js", pourcent: 70 },
+      {name: "VueX", pourcent: 80 },
+      {name: "Vuetify", pourcent: 90 },
+      {name: "Tailwind CSS", pourcent: 90 },
+      {name: "Bootstrap", pourcent: 90 },
+    ],
+    cmss: [
+      {name: "WordPress", pourcent: 85 },
+      {name: "Magento", pourcent: 50 },
+    ],
+    designs: [
+      {name: "Adobe Premiere Pro", pourcent: 85 },
+      {name: "Adobe Photoshop", pourcent: 80 },
+      {name: "Adobe InDesign", pourcent: 80 },
+      {name: "Adobe XD", pourcent: 80 },
+      {name: "Figma", pourcent: 70 },
+    ]
+  }),
   metaInfo () {
     return {
       title : 'Martial Escudero | Mon CV',
     }
   },
-
+  mounted() {
+    window.scroll(0, 0)
+  }
 }
 </script>
 
-<style>
+<style scoped>
+article {
+  background: #202020;
+  padding: 1rem;
+}
 
+.date {
+  background: #474747;
+  padding: .2rem .5rem;
+}
+
+p a {
+  color: #93C5FD;
+}
+
+p a:hover {
+  transition: .5s;
+  background: #93C5FD;
+  color: #0e0e0e;
+}
+
+button {
+  background: transparent;
+  border: none;
+}
+
+button:hover {
+  background: transparent;
+  border: none;
+}
+
+button:focus {
+  outline: unset;
+}
+
+.btn:hover {
+  transition: .5s;
+  background: #202020;
+  color: #93C5FD;
+}
+
+.btn:focus {
+  outline: unset;
+}
+
+.btn {
+  margin: 0 auto;
+  color: #202020;
+  background: #93C5FD;
+}
+
+#wrapper {
+  display: flex;
+  justify-content: center;
+}
 </style>
--- src/views/Projet.vue
+++ src/views/Projet.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto grid grid-cols-1 gap-4 sm:mt-32">
       <div class="xl:mr-10 sm:mr-4 sm:ml-4">
         <h1 class="text-center xl:text-4xl sm:text-2xl">{{projet.Titre}}</h1>
-        <h2 class="text-center xl:text-xl font-bold text-blue-300" v-for="type in projet" :key="type.item">{{type.Type}}</h2>
+        <h2 class="text-center  xl:text-xl font-bold text-blue-300" v-for="type in projet" :key="type.item">{{type.Type}}</h2>
         <h3 class="text-center xl:text-md mt-5 mb-10">{{projet.Techno}}</h3>
         <vue-markdown id="markdown" class="text-justify xl:mt-24 sm:mt-14" :source="projet.Texte"></vue-markdown>
       </div>
@@ -17,7 +17,7 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   metaInfo () {
     return {
-      title : 'Martial Escudero | Site martialescudero.com',
+      title : 'Martial Escudero | ' + this.projet.Titre,
     }
   },
   data: () => ({
