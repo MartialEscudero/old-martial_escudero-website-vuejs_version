edc720b83c726cb23f544c34e1f07327a7f0fab3
Escudero Martial <martial.escudero@gmail.com>
2021-11-22 10:41:42 +0100

    fix link

--- src/components/Home/About.vue
+++ src/components/Home/About.vue
@@ -42,7 +42,7 @@
             </a>
           </div>
           <div class="xl:mr-7">
-            <a class="flex-auto" href="https://www.link.martialescudero.com/" target="_blank">
+            <a class="flex-auto" href="https://link.martialescudero.com/" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/>
               </svg>
